c549c01b5297ce643a48012a389d124b88014e65
eshtr <42271299+eshtr@users.noreply.github.com>
2020-02-04 15:32:19 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.scss";
+import Birthday from "./birthday";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Birthday />
+    </div>
+  );
+}
